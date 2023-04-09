43ca36a12c8ef3e54afcbf20627ccda8d8e24701
Ammar <ammar_98@live.com>
2023-04-09 20:49:12 +0400

    Deployed contract

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: process.env.alchemy_https_key,
+      accounts: [`${process.env.metamask_private_key}`]
+    }
+  }
 };
 
 export default config;
--- smart_contract/scripts/deploy.ts
+++ smart_contract/scripts/deploy.ts
@@ -1,24 +1,23 @@
+import { Contract, ContractFactory } from "ethers";
 import { ethers } from "hardhat";
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+const main = async () => {
+  const Transactions: ContractFactory = await ethers.getContractFactory("Transactions");
+  const transactions: Contract = await Transactions.deploy();
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+  await transactions.deployed();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log(`Transaction deployed to : ${transactions.address}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+runMain();
