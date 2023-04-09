2ef093a43ed36e6938fc00819148c6e64a2af352
Ammar <ammar_98@live.com>
2023-04-09 13:19:50 +0400

    Added Title

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,9 @@ import './App.css';
 import { Footer, Navbar, Services, Transactions, Welcome } from "./components";
 
 function App() {
+  // Title of the app
+  document.title = "Krypto";
+
   return (
     <div className="min-h-screen">
       <div className='gradient-bg-welcome'>
