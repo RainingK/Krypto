a8ad266c2e41fee63b4a3dd4f713b0fef6e7119d
Ammar <ammar_98@live.com>
2023-04-16 22:32:42 +0400

    Changed Title

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,18 @@
 import './App.css';
 import { Footer, Navbar, Services, Transactions, Welcome } from "./components";
 
-function App() {
-  // Title of the app
-  document.title = "Krypto";
-
-  return (
-    <div className="min-h-screen">
-      <div className='gradient-bg-welcome'>
-        <Navbar />
-        <Welcome />
-      </div>
-      <Services />
-      <Transactions />
-      <Footer />
-    </div>
-
-  )
+function App () {
+	return (
+		<div className="min-h-screen">
+			<div className='gradient-bg-welcome'>
+				<Navbar />
+				<Welcome />
+			</div>
+			<Services />
+			<Transactions />
+			<Footer />
+		</div>
+	)
 }
 
 export default App
